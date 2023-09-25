741e9669fb45ae619830de9ee55693c8ecb7f4c7
Cosmin Andrei Vasilescu <c.vasilescu-1@student.tudelft.nl>
2023-09-25 21:31:09 +0200

    change user env name

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,15 @@ import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
 export const login = (req, res) => {
-    if(req.body.username === process.env.USERNAME && bcrypt.compareSync(req.body.password, process.env.PASSHASH)){
-        const token = jwt.sign({id:req.body.username}, process.env.JWTKEY)
-        return res.status(200).json({
-            "user": req.body.username,
-            "accessToken":token
-        })
-    }
-    return res.status(400).json("Wrong username or password")
+    if(req.body.username === process.env.ADMIN_USERNAME){
+        if (bcrypt.compareSync(req.body.password, process.env.PASSHASH)){
+            const token = jwt.sign({id:req.body.username}, process.env.JWTKEY)
+            return res.status(200).json({
+                "user": req.body.username,
+                "accessToken":token
+            })
+        }
+        return res.status(400).json("Incorrect password!")
+    } 
+    return res.status(400).json("Provided user does not exist!")
 }
