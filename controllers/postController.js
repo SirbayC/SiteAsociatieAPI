030c30c31af05cb14ef3c0712a390286df4f4c3b
Kocmnh <cosmin1vasilescu@yahoo.com>
2023-10-10 02:19:45 +0200

    complete crud functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -82,12 +82,11 @@ export const addPost = async (req, res) => {
     let connection;
     try {
         connection = await oracledb.getConnection();
-        console.log("writing" + req.body)
         const result = await connection.execute(
-            "INSERT INTO posts(title, content)" +
+            "INSERT INTO posts(title, content, date_created, date_updated, author)" +
             " VALUES " +
-            "(:1, :2)",
-            [req.body.title, req.body.content],
+            "(:1, :2, TO_DATE(:3, 'yyyy/mm/dd hh24:mi:ss'), TO_DATE(:4, 'yyyy/mm/dd hh24:mi:ss'),:5)",
+            [req.body.title, req.body.content, req.body.dateCreated, req.body.dateUpdated, req.body.author],
             { autoCommit: true });
         res.status(200).json("Post added!")
     } catch (err) {
@@ -105,43 +104,85 @@ export const addPost = async (req, res) => {
     return res
 }
 
-// export const deletePost = (req, res) => {
-//     const token = req.cookies.access_token
-//     if(!token) return res.status(401).json("Not Authenticated")
+export const deletePost = async (req, res) => {
+    const token = req.body.token
+    if (!token) return res.status(401).json("Not Authenticated")
+    let error = false
+    jwt.verify(token, process.env.JWTKEY, (err, userInfo) => {
+        error = err || userInfo.id != process.env.ADMIN_USERNAME
+    })
 
-//     jwt.verify(token,"jwtkey",(err,userInfo)=>{
-//         if(err) return res.status(403).json("Token is invalid!")
+    if (error)
+        return res.status(403).json("Invalid token!")
 
-//         const postId = req.params.id
-//         const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
-//         db.query(q,[postId, userInfo.id],(err,data)=>{
-//             if(err) return res.status(403).json("You can delete only your post!")
+    let connection;
+    try {
+        connection = await oracledb.getConnection();
+        const result = await connection.execute(
+            "DELETE FROM posts WHERE id = :1",
+            [req.params.id],
+            { autoCommit: true });
+        res.status(200).json(`Post with id: ${req.params.id} deleted!`)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json("Server error - db comm")
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+    return res
 
-//             return res.status(200).json("Your post has been deleted!")
-//         })
-//     })
-// }
+    if(!token) return res.status(401).json("Not Authenticated")
 
-// export const updatePost = (req, res) => {
-//     const token = req.cookies.access_token
-//     if(!token) return res.status(401).json("Not Authenticated")
+    jwt.verify(token,"jwtkey",(err,userInfo)=>{
+        if(err) return res.status(403).json("Token is invalid!")
 
-//     jwt.verify(token,"jwtkey",(err,userInfo)=>{
-//         if(err) return res.status(403).json("Token is invalid!")
+        const postId = req.params.id
+        const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+        db.query(q,[postId, userInfo.id],(err,data)=>{
+            if(err) return res.status(403).json("You can delete only your post!")
 
-//         const postId = req.params.id
-//         const q = 'UPDATE posts SET `title`=?, `desc`=?, `img`=?,`cat`=? WHERE `id` = ? AND `uid` =?'
+            return res.status(200).json("Your post has been deleted!")
+        })
+    })
+}
 
-//         const values = [
-//             req.body.title,
-//             req.body.desc,
-//             req.body.img,
-//             req.body.cat
-//         ]
+export const updatePost = async (req, res) => {
+    const token = req.body.token
+    if (!token) return res.status(401).json("Not Authenticated")
+    let error = false
+    jwt.verify(token, process.env.JWTKEY, (err, userInfo) => {
+        error = err || userInfo.id != process.env.ADMIN_USERNAME
+    })
 
-//         db.query(q, [...values, postId, userInfo.id], (err,data) => {
-//             if(err) return res.status(500).json(err)
-//             return res.status(200).json("Post has been updated")
-//         })
-//     })
-// }
+    if (error)
+        return res.status(403).json("Invalid token!")
+
+    let connection;
+    try {
+        connection = await oracledb.getConnection();
+        const result = await connection.execute(
+            "UPDATE posts SET title = :1, content = :2, date_updated = TO_DATE(:3, 'yyyy/mm/dd hh24:mi:ss')" +
+            "WHERE id = :4",
+            [req.body.title, req.body.content, req.body.dateUpdated, req.params.id],
+            { autoCommit: true });
+        res.status(200).json("Post updated!")
+    } catch (err) {
+        console.error(err);
+        res.status(500).json("Server error - db comm")
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+    return res
+}
